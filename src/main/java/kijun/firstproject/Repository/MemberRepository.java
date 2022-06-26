8344a2c2833be30f362cfb41c5ca5ff5a0f5edd0
kiiijun <snrm123@naver.com>
2022-06-26 16:40:03 +0900

    first Commit

---
+++ src/main/java/kijun/firstproject/Repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package kijun.firstproject.Repository;
+
+import kijun.firstproject.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/main/java/kijun/firstproject/service/MemberService.java
@@ -0,0 +1,45 @@
+package kijun.firstproject.service;
+
+import kijun.firstproject.Repository.MemberRepository;
+import kijun.firstproject.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+        회원가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원x
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m->{
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    public List<Member> findMembers(){
+            return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
